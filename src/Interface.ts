530d4fa5d2b7365cab7f9e5543af9690561c75a4
RobinNedergardPrivate <robinnedergard@gmail.com>
2021-09-18 11:22:39 +0200

    config fixes

--- src/Interface.ts
+++ src/Interface.ts
@@ -1,62 +1,84 @@
-'use strict';
-
 export interface Interface {
-  type: "interface" | "type"
-  name: string
-  regex: Record<string, RegExp>
-  ignore: string[]
-  properties: Property
+	type: "interface" | "type"
+	name: string
+	regex: Record<string, RegExp>
+	ignore: string[]
+	properties: Property
 }
 
 export type NamedProperty = Property & { name: string }
 export type Property =
-  | { optional?: boolean, type: "string", regex?: RegExp }
-//  | { optional?: boolean, type: "string", valid: string[] }
-  | { optional?: boolean, type: "number", validRanges?: [number, number][], regex?: RegExp } // valid?: number[] }
-  | { optional?: boolean, type: "boolean" }
-  | { optional?: boolean, type: "object", value: NamedProperty[], typeguardFunctionName?: string }
-  | { optional?: boolean, type: "array", value: Property[] }
-  | { optional?: boolean, type: "union", valid: Property[] }
+	| { optional?: boolean; type: "string"; regex?: RegExp }
+	//  | { optional?: boolean, type: "string", valid: string[] }
+	| { optional?: boolean; type: "number"; validIntegerRanges?: [number, number][]; regex?: RegExp } // valid?: number[] }
+	| { optional?: boolean; type: "boolean" }
+	| { optional?: boolean; type: "object"; value: NamedProperty[]; typeguardFunctionName?: string }
+	| { optional?: boolean; type: "array"; value: Property[] }
+	| { optional?: boolean; type: "union"; valid: Property[] }
 
 export namespace Interface {
-  export function create(value: any, name?: string): Interface | undefined {
-    return typeof value == "string" ? { name: name ?? "Unnamed", type: "type", regex: {}, ignore: [], properties: { optional: false, type: "string" } } : undefined
-  }
-  export function generateTypeguard(value: Interface): string {
-    return `export function is(value: any | ${value.name}): value is ${value.name} {
+	export function create(value: any, name?: string): Interface | undefined {
+		return typeof value == "string"
+			? {
+					name: name ?? "Unnamed",
+					type: "type",
+					regex: {},
+					ignore: [],
+					properties: { optional: false, type: "string" },
+			  }
+			: undefined
+	}
+	export function generateTypeguard(value: Interface): string {
+		return `export function is(value: any | ${value.name}): value is ${value.name} {
       return ${generateTypeguardPart("", value.properties)}
     }`
-  }
-  export function generateTypeguardPart(prefix: string, property: Property | NamedProperty): string {
-    let result: string
-    const name = ("name" in property ? property.name : "")
-    const fullName = prefix + (prefix ? "." : "") + name
-    result = "(" + property.optional ? fullName + " == undefined || " : ""
-    switch (property.type) {
-      case "array":
-        result = `Array.isArray(${fullName} && ${property.value.map(p => generateTypeguardPart(fullName, p)).join(" && ")}`
-        break
-      case "object":
-        result = `${(property.typeguardFunctionName ? property.typeguardFunctionName + "(" + fullName + ")" : `typeof ${fullName} == "object" && ${fullName}`)} && ${property.value.map(p => generateTypeguardPart(fullName, p)).join(" && ")}`
-        break;
-      case "union":
-        result = `${property.valid.join(" || ")})`
-        break;
-      case "boolean":
-        result = `false || true`
-        break;
-      case "string":
-        result = `typeof ${fullName} == "string"${property.regex ? ` && ${property.regex.source}.test(${fullName})` : ""}`
-        break;
-      case "number":
-        result = `typeof ${fullName} == "number"${property.regex ? ` && ${property.regex.source}.test(${fullName}.toString())` : ""}${property.validRanges ? ` && ${property.validRanges.map(range => `${fullName} > ${range[0]} && ${fullName} < ${range[1]}`).join(" && ")}` : ""}`
-        break;
-      default:
-        break;
-    }
-    return result + ")"
-  }
-  /*export function generateTypeCheck(fullName: string, property: Property | NamedProperty): string {
+	}
+	export function generateTypeguardPart(prefix: string, property: Property | NamedProperty): string {
+		let result: string
+		const name = "name" in property ? property.name : ""
+		const fullName = prefix + (prefix ? "." : "") + name
+		result = "(" + property.optional ? fullName + " == undefined || " : ""
+		switch (property.type) {
+			case "array":
+				result = `Array.isArray(${fullName} && ${property.value
+					.map(p => generateTypeguardPart(fullName, p))
+					.join(" && ")}`
+				break
+			case "object":
+				result = `${
+					property.typeguardFunctionName
+						? property.typeguardFunctionName + "(" + fullName + ")"
+						: `typeof ${fullName} == "object" && ${fullName}`
+				} && ${property.value.map(p => generateTypeguardPart(fullName, p)).join(" && ")}`
+				break
+			case "union":
+				result = `${property.valid.join(" || ")})`
+				break
+			case "boolean":
+				result = `false || true`
+				break
+			case "string":
+				result = `typeof ${fullName} == "string"${
+					property.regex ? ` && ${property.regex.source}.test(${fullName})` : ""
+				}`
+				break
+			case "number":
+				result = `typeof ${fullName} == "number"${
+					property.regex ? ` && ${property.regex.source}.test(${fullName}.toString())` : ""
+				}${
+					property.validIntegerRanges
+						? ` && ${property.validIntegerRanges
+								.map(range => `${fullName} > ${range[0]} && ${fullName} < ${range[1]}`)
+								.join(" && ")}`
+						: ""
+				}`
+				break
+			default:
+				break
+		}
+		return result + ")"
+	}
+	/*export function generateTypeCheck(fullName: string, property: Property | NamedProperty): string {
     let result: string
     switch (property.type) {
       case "array":
--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,41 @@
-import * as vscode from 'vscode'
+import * as vscode from "vscode"
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('extension.generateTypeguard', function () {
+	const disposable = vscode.commands.registerCommand("extension.generateTypeguard", function () {
 		// Get the active text editor
-		const editor = vscode.window.activeTextEditor;
+		const editor = vscode.window.activeTextEditor
 		//const interfaces:
-		if (editor) {
-		const document = editor.document;
-		const selection = editor.selection;
-			for (let row = 0; row < document.lineCount; row++) {
-				const line = document.lineAt(row) // document.getText()
-				const lineText = line.text
-				console.log(`Row ${row}: `, lineText)
-				const matches = lineText.match(/abc/g) ///a(?R)?z/g)
-				if (matches){
-					for (const match of matches) {
-						editor.edit(editBuilder => {
+		// if (editor) {
+		const document = editor.document
+		// const selection = editor.selection
+		for (let row = 0; row < document.lineCount; row++) {
+			const line = document.lineAt(row) // document.getText()
+			const lineText = line.text
+			console.log(`Row ${row}: `, lineText)
+			const matches = lineText.match(/abc/g) ///a(?R)?z/g)
+			if (matches) {
+				for (const match of matches) {
+					editor.edit(editBuilder => {
 						//	editBuilder.replace(line.range, "cba")
-							editBuilder.replace(new vscode.Range(new vscode.Position(row, lineText.indexOf(match)), new vscode.Position(row, lineText.indexOf(match)+match.length)), "cba");
-							//editBuilder.insert(new vscode.Position(0, 0), "hi")
-						});
-					}
+						editBuilder.replace(
+							new vscode.Range(
+								new vscode.Position(row, lineText.indexOf(match)),
+								new vscode.Position(row, lineText.indexOf(match) + match.length)
+							),
+							"cba"
+						)
+						//editBuilder.insert(new vscode.Position(0, 0), "hi")
+					})
 				}
 			}
-			// Get the word within the selection
-		//	const word = document.getText(selection);
-			//const reversed = word.split('').reverse().join('');
-			//editor.edit(editBuilder => {
-				//editBuilder.replace(selection, reversed);
-			//});
 		}
-	});
+		// Get the word within the selection
+		//	const word = document.getText(selection);
+		//const reversed = word.split('').reverse().join('');
+		//editor.edit(editBuilder => {
+		//editBuilder.replace(selection, reversed);
+		//});
+	})
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable)
 }
